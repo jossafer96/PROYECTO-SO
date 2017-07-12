bf7c601d3603cd9c4efd8c8c12ed8f687e56da9a
jossafer96 <jossafer96@gmail.com>
2017-07-11 23:24:06 -0600

    separacion

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -12,12 +12,10 @@ int main(int argc, char *argv[]) {
 	string frase;
 	string array[10];
 	
-	string instrucciones1[1000];
-	string instrucciones2[1000];
-	string instrucciones3[1000];
-	string instrucciones4[1000];
 	
-	int i=0;
+	
+	
+	
 	
 	cout << "Dime el nombre del fichero: ";
 	getline(cin,nombre);
@@ -28,73 +26,44 @@ int main(int argc, char *argv[]) {
 		
 		while (! ficheroEntrada.eof() ) {
 			getline (ficheroEntrada,frase);
-			array[i]=frase;
-			i++;
-			
-		}
-		for(int j=0;j<2;j++){
-			
-			string oracion= array[j];
+			string oracion= frase;
 			char *frase = (char*)oracion.c_str(); 
 			char *ptr;
-			cout << "Proceso: " << frase << endl;
 			ptr = strtok(frase,";");
-			int c = 0;
-			string procesos[1000];
+			int j=0;
 			while(ptr != NULL)
 			{
-				procesos[c] = ptr;
-				string oracion1=procesos[c];
-				char *frase1 = (char*)oracion1.c_str(); 
-				char *ptr1;
-				ptr1 = strtok(frase1,"/");
-				int a = 0;
-				while(ptr1 != NULL)
-				{
-					if(a==0){
-						instrucciones1[a] = ptr1;
-						ptr1 = strtok(NULL, "/");
-						a++;
-					}
-					if(a==1){
-						instrucciones2[a] = ptr1;
-						ptr1 = strtok(NULL, "/");
-						a++;
-					}
-					if(a==2){
-						instrucciones3[a] = ptr1;
-						ptr1 = strtok(NULL, "/");
-						a++;
-					}
-					if(a==3){
-						instrucciones4[a] = ptr1;
-						ptr1 = strtok(NULL, "/");
-						a++;
-					}
-					
-					
-				}
+				array[j] = ptr;
 				ptr = strtok(NULL, ";");
-				c++;
-					
-				
-				
+				j++;
 			}
 			
-			for(int x=0;x<3;x++){
-				cout<<instrucciones1[x]<<endl;
-		}
+			for(int z=0;z<j;z++){
+				string procesos;
+				procesos=array[z];
+					string oracion1=procesos;
+					cout<<oracion1<<endl;
+					char *frase1 = (char*)oracion1.c_str(); 
+					char *ptr1;
+					ptr1 = strtok(frase1,"/");
+					int a = 0;
+					while(ptr1 != NULL)
+					{
+						cout<<ptr1<<endl;
+						ptr1 = strtok(NULL,"/");
+						a++;
+					}
+					
+			}
 			
-
+			ficheroEntrada.close();
+			
+			}
+			
+		}else 
+		cout << "Fichero inexistente o faltan permisos para abrirlo" << endl;
 		
-		}
-		ficheroEntrada.close();
-	}
-	else cout << "Fichero inexistente o faltan permisos para abrirlo" << endl;  
-	
-	
 	
 
 	return 0;
 }
-
